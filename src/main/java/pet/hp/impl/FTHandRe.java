d1aaf16d5c8829602f362ec4eb517fa7623cf937
Pavel Sakun <pavel.sakun@gmail.com>
2015-08-16 15:47:26 +0300

    Update FTP hand regular expression to format as of Aug'15

--- src/main/java/pet/hp/impl/FTHandRe.java
+++ src/main/java/pet/hp/impl/FTHandRe.java
@@ -7,12 +7,13 @@ final class FTHandRe {
 	static final int hid = 1;
 	static final int tname = 2, tid = 3;
 	static final int table = 4, tabletype = 5;
-	static final int sb = 6, bb = 7, ante = 8;
-	static final int lim = 9;
-	static final int game = 10;
+	static final int lim = 6;
+	static final int game = 7;
+	static final int sb = 8, bb = 9, ante = 10;
 	static final int date1 = 11;
 	static final int date2 = 12;
 	static final int partial = 13;
+
 	static final String re = 
 			// Full Tilt Poker Game #31364220549:
 			"Full Tilt Poker Game #(\\d+):"
@@ -20,12 +21,12 @@ final class FTHandRe {
 			+ "(?: (.+?) \\((\\d+)\\),)?"
 			// Table .COM Play 2    (6 max, ante, deep)
 			+ " Table (.+?)(?: \\((.+?)\\))?"
-			// - 300/600 Ante 100
-			+ " - (\\S+)/(\\S+)(?: Ante (\\S+))?"
-			// No Limit  
-			+ " - (Limit|Pot Limit|No Limit)"
+			// No Limit
+			+ " - (Limit|PL|NL)"
 			// 2-7 Triple Draw
 			+ " (.+)"
+			// - 300/600 Ante 100
+			+ " - (\\S+)/(\\S+)(?: Ante (\\S+))?"
 			// 20:01:13 UTC - 2012/11/05
 			+ " - (\\d+:\\d+:\\d+ \\w+ - \\d+/\\d+/\\d+)"
 			// [02:07:17 ET - 2012/11/10]
